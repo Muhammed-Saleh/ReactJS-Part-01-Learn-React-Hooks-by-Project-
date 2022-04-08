9d012067658b6d79b6936f911c9a5aa78e3a0778
Muhammed-Saleh <hamada500005@gmail.com>
2022-04-08 00:03:27 -0700

    Fetch Data inside JSX with MAP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM  from "react-dom";
+import App from './components/App';
 
 
 ReactDOM.render(
-  <div>hello world</div>,
+<App/>,
   document.getElementById('root')
 );
